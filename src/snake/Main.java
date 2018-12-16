f54dd40768b72076c1cbad0b74cef0c8e6dcc5a6
Devin <devinfrench@users.noreply.github.com>
2018-12-15 16:36:45 -0800

    Init application

---
+++ src/snake/Main.java
@@ -0,0 +1,32 @@
+package snake;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 600;
+
+    private GraphicsContext graphics;
+
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        graphics = canvas.getGraphicsContext2D();
+        canvas.setFocusTraversable(true);
+        root.getChildren().add(canvas);
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
